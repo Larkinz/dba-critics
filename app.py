73ed5472a410f388145d2ba3b49c457089017bbc
larkinz <andy_larkin33@hotmail.com>
2021-05-22 22:23:14 +0200

    feat: added required code to app.py to connect with mongoDB

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get(
+    "MONGO_DBNAME")  # grab the database name
+app.config["MONGO_URI"] = os.environ.get(
+    "MONGO_URI")  # grab the connection string
+app.secret_key = os.environ.get("SECRET_KEY")  # grab the secret key
+
+# setup an instance of PyMongo
+mongo = PyMongo(app)
+
 
 @app.route("/")
+@app.route("/welcome")
 def welcome():
     return render_template("welcome.html")
 
