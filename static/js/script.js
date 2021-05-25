0c451a247fcb32d35057a8cf20784ad398526ac6
larkinz <andy_larkin33@hotmail.com>
2021-05-25 04:06:19 +0200

    feat: added parallax js functionality

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // initialize materialize sidenav
     const SIDENAV = document.querySelectorAll(".sidenav");
     M.Sidenav.init(SIDENAV, {});
+
+    // initialize materialize parralax
+    const PARALLAX = document.querySelectorAll(".parallax");
+    M.Parallax.init(PARALLAX, {});
 });
