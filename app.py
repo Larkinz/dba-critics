77e7d187f84c000d077c7f1898aa8d6a8a14ccd4
larkinz <andy_larkin33@hotmail.com>
2021-05-31 03:09:38 +0200

    feat: created log in functionality

--- app.py
+++ app.py
@@ -37,8 +37,29 @@ def albums():
     return render_template("albums.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if username exists in database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username")})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username")
+                return redirect(url_for("albums"))
+            else:
+                # invalid password match
+                flash("Incorrect username and/or password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect username and/or password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
