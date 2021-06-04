291f4be77502cfe6be5e207cf1b19599a7b43152
larkinz <andy_larkin33@hotmail.com>
2021-06-04 03:52:53 +0200

    feat: added JS range type input sliders functionality

--- static/js/script.js
+++ static/js/script.js
@@ -26,3 +26,39 @@ document.addEventListener("DOMContentLoaded", function () {
         endingTop: '25%'
     });
 });
+
+// album 001 range input slider functionality
+/* credits #?? (see README.md credits section) */
+const RANGE_INPUT_ALBUM_001 = document.getElementById("slider-album-001");
+let rangeOutputAlbum001 = document.getElementById("your-album-001-rating");
+
+RANGE_INPUT_ALBUM_001.oninput = function () {
+    rangeOutputAlbum001.innerHTML = this.value;
+}
+
+// album 002 range input slider functionality
+/* credits #?? (see README.md credits section) */
+const RANGE_INPUT_ALBUM_002 = document.getElementById("slider-album-002");
+let rangeOutputAlbum002 = document.getElementById("your-album-002-rating");
+
+RANGE_INPUT_ALBUM_002.oninput = function () {
+    rangeOutputAlbum002.innerHTML = this.value;
+}
+
+// album 003 range input slider functionality
+/* credits #?? (see README.md credits section) */
+const RANGE_INPUT_ALBUM_003 = document.getElementById("slider-album-003");
+let rangeOutputAlbum003 = document.getElementById("your-album-003-rating");
+
+RANGE_INPUT_ALBUM_003.oninput = function () {
+    rangeOutputAlbum003.innerHTML = this.value;
+}
+
+// album 004 range input slider functionality
+/* credits #?? (see README.md credits section) */
+const RANGE_INPUT_ALBUM_004 = document.getElementById("slider-album-004");
+let rangeOutputAlbum004 = document.getElementById("your-album-004-rating");
+
+RANGE_INPUT_ALBUM_004.oninput = function () {
+    rangeOutputAlbum004.innerHTML = this.value;
+}
